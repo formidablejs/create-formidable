ce295cbd65c754e670bd53a7449f025881f2f42f
Donald Pakkies <donaldpakkies@gmail.com>
2023-05-20 18:42:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Create Formidable App
+A TypeScript & Imba Full-stack Framework for Nodejs.
+
+The `create-formidable-app` CLI tool is a command-line interface tool that allows you to install Formidable in a fresh project. This installer is built on top of the [Formidable Installer](https://github.com/formidablejs/installer) package.
+
+![npm](https://img.shields.io/npm/v/@formidablejs/installer)
+![GitHub](https://img.shields.io/github/license/formidablejs/installer)
+
+## Official Documentation
+
+Documentation for installing Formidable can be found on the [Formidable website](https://www.formidablejs.org/docs#your-first-formidable-application).
+
+## Contributing
+
+Thank you for considering contributing to the Formidable framework! The contribution guide can be found in the [Formidable documentation](https://www.formidablejs.org/docs/contributions).
+
+## Code of Conduct
+
+In order to ensure that the Formidable community is welcoming to all, please review and abide by the [Code of Conduct](https://www.formidablejs.org/docs/contributions#code-of-conduct).
+
+## Security Vulnerabilities
+
+If you discover a security vulnerability within Formidable, please send an e-mail to Donald Pakkies via [donaldpakkies@gmail.com](mailto:donaldpakkies@gmail.com). All security vulnerabilities will be promptly addressed.
+
+## License
+
+The Formidable framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ src/commands/index.ts
@@ -0,0 +1,55 @@
+import { Args, Command, Flags } from '@oclif/core'
+import { input } from '@inquirer/prompts'
+import { run } from '@formidablejs/installer'
+import path = require('path')
+
+export default class Create extends Command {
+    static description = 'Create a new Formidable application'
+
+    static args = {
+        name: Args.string({ description: 'Application name', required: false }),
+    }
+
+    static flags = {
+        'silent-install': Flags.boolean({ description: 'Install silently', char: 'q' }),
+        'sqlite-git-ignore': Flags.boolean({ description: 'Add SQLite Database to gitignore', char: 'G' }),
+        'use-pnpm': Flags.boolean({ description: 'Use pnpm instead of npm or yarn', char: 'p' }),
+        'use-npm': Flags.boolean({ description: 'Use npm instead of pnpm or yarn', char: 'n' }),
+        'use-yarn': Flags.boolean({ description: 'Use yarn instead of pnpm or npm', char: 'y' }),
+        database: Flags.string({ description: 'The default database driver to use', char: 'd', options: ['MySQL', 'PostgreSQL', 'SQLite', 'MSSQL', 'Oracle', 'skip'] }),
+        dev: Flags.boolean({ description: 'Use dev branch' }),
+        git: Flags.boolean({ description: 'Initialize a Git repository', char: 'g' }),
+        imba: Flags.boolean({ description: 'Create Imba Full-Stack application' }),
+        language: Flags.string({ description: 'The default language to use', char: 'l', options: ['imba', 'typescript'] }),
+        manager: Flags.string({ description: 'The default package manager to use', char: 'm', options: ['npm', 'pnpm', 'yarn'] }),
+        react: Flags.boolean({ description: 'Create React Full-Stack application' }),
+        scaffolding: Flags.string({ description: 'The default scaffolding to use', char: 'S', options: ['mpa', 'spa'] }),
+        stack: Flags.string({ description: 'The default stack to use', char: 's', options: ['imba', 'react', 'svelte', 'vue'] }),
+        svelte: Flags.boolean({ description: 'Create Svelte Full-Stack application' }),
+        type: Flags.string({ description: 'The type of application to create', char: 't', options: ['api', 'full-stack'] }),
+        vue: Flags.boolean({ description: 'Create Vue Full-Stack application' }),
+    }
+
+    async run(): Promise<void> {
+        const { args, flags } = await this.parse(Create)
+
+        if (!args.name) {
+            const name = await input({
+                message: 'What is the name of your application?',
+                default: path.basename(process.cwd())
+            });
+
+            args.name = name
+        }
+
+        const installerArgs = ['new', args.name]
+
+        for (const [key, value] of Object.entries(flags)) {
+            if (value) {
+                installerArgs.push(`--${key}`)
+            }
+        }
+
+        await run(installerArgs)
+    }
+}
